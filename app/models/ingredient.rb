452d1e48bc97b2b75aeb87ddd53b668696aaf044
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2018-02-15 12:18:52 +0100

    models and DB

---
+++ app/models/ingredient.rb
@@ -0,0 +1,18 @@
+class Ingredient < ApplicationRecord
+  has_many :doses
+  validates :name, uniqueness: true, presence: true
+
+  before_destroy :check_for_doses
+
+  private
+
+  def check_for_doses
+    if self.doses.exists?
+      # errors.add_to_base("cannot delete ingredients if they are used by a cocktail")
+      return false
+    end
+  end
+
+end
+
+
